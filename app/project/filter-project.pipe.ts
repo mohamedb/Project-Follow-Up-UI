2e439eca9c21eab09920bba654cd3d1ec9e45929
mohamedb <mohamed.boullouz@gmail.com>
2016-04-05 23:46:24 +0200

    Finish the filter

--- app/project/filter-project.pipe.ts
+++ app/project/filter-project.pipe.ts
@@ -6,8 +6,17 @@ import {Pipe, PipeTransform} from 'angular2/core';
 })
 
 export class FilterProjectPipe implements PipeTransform {
-    transform(items: any[], args: any[]) : any {
-        
-       return items.filter(item => item.type == "CHAMBRE");
+    transform(items: any[], args: any[]): any {
+        let argValue= parseInt(args[0]);
+        switch (argValue) {
+            case -1:
+                return items;
+            case 0:
+                return items.filter(item => item.Over == false);
+            case 1:
+                return items.filter(item => item.Over == true);
+            default:
+                return items;
+        } 
     }
 }
