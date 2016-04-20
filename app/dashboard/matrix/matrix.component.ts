a7fcdbf7604ce9b900aa824503d73118fdf5693f
mohamedb <mohamed.boullouz@gmail.com>
2016-04-20 21:04:01 +0200

    Matrix skelton

---
+++ app/dashboard/matrix/matrix.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from 'angular2/core';
+
+@Component({
+    selector: 'matrix',
+    templateUrl: 'app/dashboard/matrix/matrix.component.html'
+})
+export class MatrixComponent implements OnInit {
+    constructor() { }
+ 
+    ngOnInit() { }
+
+}
