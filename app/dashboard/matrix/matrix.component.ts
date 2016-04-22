7bf80c5037ab599542fbf985cf48e4f1253d2bca
mohamedb <mohamed.boullouz@gmail.com>
2016-04-22 23:33:15 +0200

    Add a matrix model

--- app/dashboard/matrix/matrix.component.ts
+++ app/dashboard/matrix/matrix.component.ts
@@ -1,16 +1,12 @@
 import { Component, OnInit, Input } from 'angular2/core';
+import { MatrixModel } from './matrix.model';
 
 @Component({
     selector: 'matrix',
     templateUrl: 'app/dashboard/matrix/matrix.component.html'
 })
 export class MatrixComponent implements OnInit {
-    @Input("matrixData") Model: any = {
-        ImportantAndUrgent: [],
-        ImportantNotUrgent: [],
-        NotImportantButUrgent: [],
-        NotImportantNotUrgent: []
-    };
+    @Input("matrixData") Model:MatrixModel;
     constructor() { }
 
     ngOnInit() {
---
+++ app/dashboard/matrix/matrix.model.ts
@@ -0,0 +1,10 @@
+/**
+ * Matrix model
+ */
+export class MatrixModel {
+    ImportantAndUrgent: Array<any>[];
+    ImportantNotUrgent: Array<any>[];
+    NotImportantButUrgent: Array<any>[];
+    NotImportantNotUrgent: Array<any>[];
+
+}
