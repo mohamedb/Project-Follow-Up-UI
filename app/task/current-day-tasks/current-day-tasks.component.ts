9aba2a410481e0f04f5f65d67ab8d494cdda171a
mohamedb <mohamed.boullouz@gmail.com>
2016-04-24 14:11:59 +0200

    Mark current task done, use same api as change-state

--- app/task/current-day-tasks/current-day-tasks.component.ts
+++ app/task/current-day-tasks/current-day-tasks.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit, Input } from 'angular2/core';
+import {TaskService} from './../task.service'
 
 @Component({
     selector: 'current-day-tasks',
-    templateUrl: 'app/task/current-day-tasks/current-day-tasks.component.html'
+    templateUrl: 'app/task/current-day-tasks/current-day-tasks.component.html',
+    providers:[TaskService]
 })
 export class CurrentDayTasksComponent implements OnInit {
     @Input("CurrentDayTasks") CurrentDayTasks:any;
     @Input("Title") Title:any;
-    constructor() { }
+    constructor(private taskService:TaskService) { }
 
     ngOnInit() { }
+    
+    markDone(taskId:number){
+        this.taskService.changeStateTask(taskId).subscribe(
+            res=>alert(res.Message)
+        )
+    }
 
 }
