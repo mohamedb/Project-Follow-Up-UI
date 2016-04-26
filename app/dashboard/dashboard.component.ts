d8b337e8e601816db0af95c52e5ea73b23878ce6
mohamedb <mohamed.boullouz@gmail.com>
2016-04-26 20:44:21 +0200

    add model state calidation to event emiter response

--- app/dashboard/dashboard.component.ts
+++ app/dashboard/dashboard.component.ts
@@ -3,16 +3,18 @@ import { DashboardService} from './dashboard.service';
 import {MatrixComponent } from './matrix/matrix.component';
 import {TaskPlanningComponent} from './../task/task-planning/task-planning.component';
 import {CurrentDayTasksComponent} from './../task/current-day-tasks/current-day-tasks.component';
-
+import {ModelStateComponent} from './../shared/model-state/model-state.component';
+import {ModelState} from './../shared/model-state/model-state.model';
 @Component({
     selector: 'dashboard',
     templateUrl: 'app/dashboard/dashboard.component.html',
-    directives: [MatrixComponent,TaskPlanningComponent,CurrentDayTasksComponent],
+    directives: [MatrixComponent,TaskPlanningComponent,CurrentDayTasksComponent,ModelStateComponent],
     providers: [DashboardService]
 })
 export class DashboardComponent implements OnInit {
     data: any = {};
     matrix:any;
+     modelState:ModelState=new ModelState();
     constructor(private dashboardService: DashboardService) { }
 
     ngOnInit() {
@@ -26,7 +28,9 @@ export class DashboardComponent implements OnInit {
         this.matrix=res.MatrixVM;
     }
     handleResponseEvent(response){
+         this.modelState= <ModelState>response;
         if(response.State==1  )  {
+          
             this.ngOnInit(); //reload 
         }
         else {
