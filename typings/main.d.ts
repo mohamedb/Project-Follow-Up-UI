99a4363b2cf3396abdad0a784939fd936bd976c3
mohamedb <mohamed.boullouz@gmail.com>
2016-05-10 19:46:34 +0200

    Add typed Moment js lib

--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="jquery/jquery.d.ts" />
+/// <reference path="moment/moment.d.ts" />
 /// <reference path="main/ambient/es6-shim/index.d.ts" />
 /// <reference path="main/ambient/jasmine/index.d.ts" />
 /// <reference path="theme.d.ts" />
